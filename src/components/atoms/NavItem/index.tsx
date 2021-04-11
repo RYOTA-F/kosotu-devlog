3e0f0ce556b90e7d9624030cd58c4dcbcde97daa
Ryota Fujishima <r.fujishima@comnico.jp>
2021-04-11 21:14:34 +0900

    develop: Header 追加

---
+++ src/components/atoms/NavItem/index.tsx
@@ -0,0 +1,35 @@
+/**
+ * atoms/LinkItem
+ * Atoms
+ * @package Component
+ */
+import React from 'react'
+/* components */
+import Link from 'next/link'
+/* styles */
+import styles from './styles.module.scss'
+
+/**
+ * props
+ */
+interface IProps {
+  title: string
+  link: string
+}
+
+/**
+ * container
+ * @params prop
+ * @returns
+ */
+const NavItem: React.FC<IProps> = ({title, link} ) => {
+  return (
+    <div className={styles.container}>
+      <Link href={link}>
+        <div className={styles.title}>{title}</div>
+      </Link>
+    </div>
+  )
+}
+
+export default NavItem
--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -4,6 +4,8 @@
  * @package Component
  */
 import React from 'react'
+/* components */
+import NavItem from '@/components/atoms/NavItem'
 /* styles */
 import styles from './styles.module.scss'
 
@@ -13,10 +15,33 @@ import styles from './styles.module.scss'
  * @returns
  */
 const Header: React.FC = () => {
+  const navLinks = [
+    {
+      title: 'フロントエンド',
+      link: '/category/front-end'
+    },
+    {
+      title: 'バックエンド',
+      link: '/category/front-end'
+    },
+    {
+      title: 'プロフィール',
+      link: '/category/front-end'
+    },
+    {
+      title: 'お問い合わせ',
+      link: '/category/front-end'
+    },
+  ]
+
   return (
     <div className={styles.container}>
-      <div className={styles.top}/>
-
+      <div className={styles.top} />
+      <div className={styles.nav_links}>
+        {navLinks.map((v) => (
+          <NavItem title={v.title} link={v.link}/>
+        ))}
+      </div>
     </div>
   )
 }
