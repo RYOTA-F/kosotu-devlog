8ecc5b9a89a29b65f0afce0e522601211c304378
Ryota Fujishima <r.fujishima@comnico.jp>
2021-04-11 18:07:07 +0900

    develop: paths 解決

---
+++ src/components/templates/TopLayout/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * templates/TopLayout
+ * Templates
+ * @package Component
+ */
+import React from 'react'
+
+/**
+ * container
+ * @param prop
+ * @returns
+ */
+const TopLayout: React.FC = () => {
+  return (
+    <div>TopLayout</div>
+  )
+}
+
+export default TopLayout
