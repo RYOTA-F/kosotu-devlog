cf7c9ee62c9213818903a5950255290f9a209374
Ryota Fujishima <r.fujishima@comnico.jp>
2021-06-05 02:06:44 +0900

    develop: 再調整

--- src/components/templates/BlogArticleLayout/index.tsx
+++ src/components/templates/BlogArticleLayout/index.tsx
@@ -41,8 +41,8 @@ const BlogArticleLayout: React.FC<BlogArticleLayoutProps> = (props: BlogArticleL
         <div className={styles.sideBar}>
           <SideProfile />
           <div className={styles.sideBar__item}>
-            <div>サイドカテゴリー</div>
-            {/* <SideCategories categories={categories} /> */}
+            {/* <div>サイドカテゴリー</div> */}
+            <SideCategories categories={categories} />
           </div>
         </div>
       </div>
