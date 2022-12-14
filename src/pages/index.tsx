d77b25f4119cedb8142e8a9233aa425b8912879b
RYOTA-F <ry.fj0304@gmail.com>
2022-12-15 01:43:44 +0900

    :wrench: UPDATE ESLint Rule

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -69,3 +69,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
