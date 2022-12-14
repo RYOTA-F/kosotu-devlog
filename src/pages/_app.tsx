656f61e81987178e2408da6e35b9ba38d76b6676
RYOTA-F <ry.fj0304@gmail.com>
2022-12-15 02:00:11 +0900

    :recycle: UPDATE Top CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+/* styles */
+import './styles/normalize.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
