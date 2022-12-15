0826a0fcbc1ed198faa34b8e208571980cae6d95
RYOTA-F <ry.fj0304@gmail.com>
2022-12-16 06:56:47 +0900

    :bug: Build

--- src/sections/Header/index.test.tsx
+++ src/sections/Header/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import Header, { ARIA_LABEL } from './'
