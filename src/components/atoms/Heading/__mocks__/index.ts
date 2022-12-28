69c2e827113ed8ccbcb968927daf6331b1ce305f
RYOTA-F <ry.fj0304@gmail.com>
2022-12-29 03:34:14 +0900

    :sparkles: H2

--- src/components/atoms/Heading/__mocks__/index.ts
+++ src/components/atoms/Heading/__mocks__/index.ts
@@ -1,3 +1,1 @@
-export const defaultProps = {
-  children: 'テスト',
-}
+export const defaultProps = 'テスト'
--- src/components/atoms/Heading/const.ts
+++ src/components/atoms/Heading/const.ts
@@ -1,3 +1,5 @@
 export const ARIA_LABEL = {
+  H1: 'h1',
+  H2: 'h2',
   H4: 'h4',
 } as const
