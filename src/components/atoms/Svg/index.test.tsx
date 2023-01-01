09bb5491ed94f662b5863fa58671f5ef5f6c2f87
RYOTA-F <ry.fj0304@gmail.com>
2023-01-01 17:40:26 +0900

    :sparkles: ListSvg

--- src/components/atoms/Svg/index.test.tsx
+++ src/components/atoms/Svg/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import { TimeSvg, TIME_SVG_ARIA_LABEL } from '.'
+import { TimeSvg, TIME_SVG_ARIA_LABEL, ListSvg, LIST_SVG_ARIA_LABEL } from '.'
 import { defaultProps } from './__mocks__'
 
 describe('Svg', () => {
@@ -11,6 +11,15 @@ describe('Svg', () => {
 
       const timeSvgElement = screen.getByLabelText(TIME_SVG_ARIA_LABEL)
       expect(timeSvgElement).toBeInTheDocument()
+    })
+  })
+
+  describe('ListSvg', () => {
+    test('描画される', () => {
+      render(<ListSvg {...defaultProps} />)
+
+      const listSvgElement = screen.getByLabelText(LIST_SVG_ARIA_LABEL)
+      expect(listSvgElement).toBeInTheDocument()
     })
   })
 })
