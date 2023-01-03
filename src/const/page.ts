b9ae7493919d772ebdc62ef2944b6a5ac0ce2730
RYOTA-F <ry.fj0304@gmail.com>
2023-01-04 01:50:52 +0900

    :sparkles: TagItem

--- src/const/page.ts
+++ src/const/page.ts
@@ -2,6 +2,7 @@ export const PAGE = {
   ROOT: '/',
   BLOGS: '/blogs',
   CATEGORIES: '/categories',
+  TAGS: '/tags',
   PROFILE: '/profile',
   SITE_MAP: '/sitemap',
   PRIVACY_POLICY: '/privacy-policy',
