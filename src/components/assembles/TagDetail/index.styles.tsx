3a57da7999fe6f438ad16c452afdca1186680321
RYOTA-F <ry.fj0304@gmail.com>
2023-01-04 23:59:52 +0900

    :sparkles: TagDetail

---
+++ src/components/assembles/TagDetail/index.styles.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+/* Const */
+import { SIZE } from '@/const/index'
+
+export const TagDetailWrapper = styled.section``
+
+export const BlogCardListWrapper = styled.div`
+  margin-top: ${SIZE.SPACE.X40};
+`
