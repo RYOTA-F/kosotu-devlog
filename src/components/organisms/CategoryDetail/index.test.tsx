dbf51f92c9cd77883848220bccf9a2d26034d544
RYOTA-F <ry.fj0304@gmail.com>
2023-01-08 11:40:59 +0900

    :sparkles: CategoryDetail Test

--- src/components/organisms/CategoryDetail/index.test.tsx
+++ src/components/organisms/CategoryDetail/index.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import CategoryDetail, { ARIA_LABEL } from '.'
+import { ARIA_LABEL as CATEGORY_DETAIL_HEADER_ARIA_LABEL } from '@/components/organisms/CategoryDetailHeader'
+import { ARIA_LABEL as BLOG_CARD_LIST_ARIA_LABEL } from '@/components/organisms/BlogCardList'
 import CategoryContextProviderMock from '@/stores/__mocks__/category'
 
 describe('CategoryDetail', () => {
@@ -20,6 +22,21 @@ describe('CategoryDetail', () => {
     expect(categoryDetailElement).toBeInTheDocument()
   })
 
-  test.todo('ヘッダー')
-  test.todo('投稿一覧')
+  describe('CategoryDetailHeader', () => {
+    test('描画される', () => {
+      const categoryDetailHeaderElement = screen.getByLabelText(
+        CATEGORY_DETAIL_HEADER_ARIA_LABEL.CATEGORY_DETAIL_HEADER
+      )
+      expect(categoryDetailHeaderElement).toBeInTheDocument()
+    })
+  })
+
+  describe('BlogCardList', () => {
+    test('描画される', () => {
+      const blogCardListElement = screen.getByLabelText(
+        BLOG_CARD_LIST_ARIA_LABEL.BLOG_CARD_LIST
+      )
+      expect(blogCardListElement).toBeInTheDocument()
+    })
+  })
 })
