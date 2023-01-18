d62f86ccfc085a80f5fcd65af944486a368ce969
RYOTA-F <ry.fj0304@gmail.com>
2023-01-18 15:52:55 +0900

    :rotating_light: useBlogData  Test

---
+++ src/hooks/useBlogData/index.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { renderHook, act } from '@testing-library/react'
+
+import useBlogData from './'
+import { initialBlogState, BLOG_ACTION_TYPES } from '@/stores/blog'
+import { blogsStateMock } from '@/stores/__mocks__/blog/mock'
+
+const dispatchMock = jest.fn()
+jest.spyOn(React, 'useContext').mockImplementation(() => ({
+  state: {
+    ...initialBlogState,
+    blogs: blogsStateMock,
+  },
+  dispatch: dispatchMock,
+}))
+
+describe('useBlogData', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('blog', () => {
+    test('初期値がセットされている', () => {
+      const { result } = renderHook(() => useBlogData())
+
+      expect(result.current.blog).toEqual(blogsStateMock[0])
+    })
+  })
+
+  describe('blogs', () => {
+    test('初期値がセットされている', () => {
+      const { result } = renderHook(() => useBlogData())
+
+      expect(result.current.blogs).toEqual(blogsStateMock)
+    })
+  })
+
+  describe('setBlogs', () => {
+    test('dispatch に適切な引数が渡される', () => {
+      const { result } = renderHook(() => useBlogData())
+      act(() => result.current.setBlogs(blogsStateMock))
+
+      expect(dispatchMock).toBeCalled()
+      expect(dispatchMock).toBeCalledWith({
+        type: BLOG_ACTION_TYPES.UPDATE_BLOGS,
+        payload: blogsStateMock,
+      })
+    })
+  })
+
+  describe('resetBlogs', () => {
+    test('dispatch に適切な引数が渡される', () => {
+      const { result } = renderHook(() => useBlogData())
+      act(() => result.current.resetBlogs())
+
+      expect(dispatchMock).toBeCalled()
+      expect(dispatchMock).toBeCalledWith({
+        type: BLOG_ACTION_TYPES.UPDATE_BLOGS,
+        payload: [],
+      })
+    })
+  })
+})
