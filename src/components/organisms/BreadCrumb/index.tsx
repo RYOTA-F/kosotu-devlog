05c8b0d5eba185122cf41af4c4e1b1f4f498bde7
RYOTA-F <ry.fj0304@gmail.com>
2023-02-03 00:40:59 +0900

    :art: BreadCrumb

--- src/components/organisms/BreadCrumb/index.tsx
+++ src/components/organisms/BreadCrumb/index.tsx
@@ -7,16 +7,18 @@ import { PAGE } from '@/const/index'
 import { BREAD_CRUMB, ARIA_LABEL } from './const'
 /* Hooks */
 import useCommonData from '@/hooks/useCommonData'
+import useMediaQuery from '@/hooks/useMediaQuery'
 /* Styles */
 import { BreadCrumbWrapper, ListItem, Title, HomeWrapper } from './index.styles'
 
 const BreadCrumb: FC = () => {
   const { breadCrumb } = useCommonData()
+  const { isSP } = useMediaQuery()
 
   return (
     <>
       {breadCrumb && (
-        <BreadCrumbWrapper aria-label={ARIA_LABEL.BREAD_CRUMB}>
+        <BreadCrumbWrapper isSP={isSP} aria-label={ARIA_LABEL.BREAD_CRUMB}>
           <ListItem>
             <Link href={PAGE.ROOT}>
               <HomeSvg
