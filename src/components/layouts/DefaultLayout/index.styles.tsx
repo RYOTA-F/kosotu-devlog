5313bcf36096c8a843649cc7cd1bf068d9ca0c60
RYOTA-F <ry.fj0304@gmail.com>
2023-02-03 00:15:44 +0900

    :recycle: DefaultLayout

--- src/components/layouts/DefaultLayout/index.styles.tsx
+++ src/components/layouts/DefaultLayout/index.styles.tsx
@@ -6,13 +6,13 @@ export const MainWrapper = styled.div<{ isPC: boolean }>`
   ${({ isPC }) =>
     isPC
       ? `
-    display: flex;
-    justify-content: space-between;
-    padding: ${SIZE.SPACE.X60} 10%;
-  `
+        display: flex;
+        justify-content: space-between;
+        padding: ${SIZE.SPACE.X60} 10%;
+      `
       : `
-    padding: ${SIZE.SPACE.X12};
-  `}
+        padding: ${SIZE.SPACE.X12};
+      `}
 `
 
 export const Main = styled.main`
