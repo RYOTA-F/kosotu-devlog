2857ab85dafbb32f6a649c0dc90437b0971a96ae
RYOTA-F <ry.fj0304@gmail.com>
2023-02-03 02:30:56 +0900

    :art: BlogDetailHeader

--- src/components/organisms/BlogDetailHeader/index.styles.tsx
+++ src/components/organisms/BlogDetailHeader/index.styles.tsx
@@ -4,36 +4,51 @@ import { COLOR, SIZE } from '@/const/index'
 
 export const BlogDetailHeaderWrapper = styled.section``
 
-export const Title = styled.h1`
+export const Title = styled.h1<{ isSP: boolean }>`
   color: ${COLOR.GRAY_SCALE.GRAY.TEXT_1};
-  font-size: ${SIZE.FONT.X24};
+  font-size: ${({ isSP }) => (isSP ? SIZE.FONT.W5 : SIZE.FONT.X24)};
 `
 
-export const DataWrapper = styled.div`
-  display: flex;
-  align-items: center;
+export const DataWrapper = styled.div<{ isSP: boolean }>`
+  ${({ isSP }) =>
+    !isSP &&
+    `
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+    `}
   margin-top: ${SIZE.SPACE.X16};
 `
 
-export const TagListWrapper = styled.div`
-  margin-left: ${SIZE.SPACE.X20};
+export const CategoryListWrapper = styled.div`
+  margin-right: ${SIZE.SPACE.X20};
 `
 
-export const DateWrapper = styled.div`
+export const TagListWrapper = styled.div<{ isSP: boolean }>`
+  margin-right: ${SIZE.SPACE.X32};
+  ${({ isSP }) => isSP && `margin-top: ${SIZE.SPACE.X4}`};
+`
+
+export const DateWrapper = styled.div<{ isSP: boolean }>`
   display: flex;
   align-items: center;
-  margin-left: ${SIZE.SPACE.X32};
   margin-bottom: ${SIZE.SPACE.X4};
+  ${({ isSP }) => isSP && `margin-top: ${SIZE.SPACE.X4}`};
 `
 
-export const Date = styled.time`
+export const Date = styled.time<{ isSP: boolean }>`
   margin-left: ${SIZE.SPACE.X4};
   color: ${COLOR.GRAY_SCALE.GRAY.TEXT_3};
-  font-size: ${SIZE.FONT.X16};
+  font-size: ${({ isSP }) => (isSP ? SIZE.FONT.W3 : SIZE.FONT.X16)};
 `
 
 export const ImageWrapper = styled.figure`
   margin-top: ${SIZE.SPACE.X20};
+
+  img {
+    width: 100%;
+    height: auto;
+  }
 `
 
 export const WelcomeMessageWrapper = styled.div`
@@ -52,7 +67,7 @@ export const WelcomeMessageDescription = styled.p`
   line-height: 2.2;
 `
 
-export const TableOfContentsWrapper = styled.div`
-  width: 90%;
+export const TableOfContentsWrapper = styled.div<{ isSP: boolean }>`
+  width: ${({ isSP }) => (isSP ? '100%' : '90%')};
   margin: ${SIZE.SPACE.X32} auto;
 `
