6948e050c508596853dc0b53cd1a97a2f4ea0771
RYOTA-F <ry.fj0304@gmail.com>
2023-02-07 00:50:09 +0900

    :rotating_light: Header

--- src/components/organisms/Header/index.test.tsx
+++ src/components/organisms/Header/index.test.tsx
@@ -2,14 +2,55 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import Header, { ARIA_LABEL } from '.'
+import { MediaQueryContext } from '@/stores/mediaQuery'
 
 describe('Header', () => {
   beforeEach(() => {
-    render(<Header />)
+    jest.clearAllMocks()
   })
 
-  test('描画される', () => {
-    const headerElement = screen.getByLabelText(ARIA_LABEL.HEADER)
-    expect(headerElement).toBeInTheDocument()
+  describe('PC', () => {
+    test('描画される', () => {
+      render(
+        <MediaQueryContext.Provider
+          value={{ isPC: true, isTB: false, isSP: false }}
+        >
+          <Header />
+        </MediaQueryContext.Provider>
+      )
+
+      const headerElement = screen.getByLabelText(ARIA_LABEL.HEADER)
+      expect(headerElement).toBeInTheDocument()
+    })
+  })
+
+  describe('TB', () => {
+    test('描画される', () => {
+      render(
+        <MediaQueryContext.Provider
+          value={{ isPC: false, isTB: true, isSP: false }}
+        >
+          <Header />
+        </MediaQueryContext.Provider>
+      )
+
+      const headerElement = screen.getByLabelText(ARIA_LABEL.HEADER)
+      expect(headerElement).toBeInTheDocument()
+    })
+  })
+
+  describe('SP', () => {
+    test('描画される', () => {
+      render(
+        <MediaQueryContext.Provider
+          value={{ isPC: false, isTB: false, isSP: true }}
+        >
+          <Header />
+        </MediaQueryContext.Provider>
+      )
+
+      const headerElement = screen.getByLabelText(ARIA_LABEL.HEADER)
+      expect(headerElement).toBeInTheDocument()
+    })
   })
 })
