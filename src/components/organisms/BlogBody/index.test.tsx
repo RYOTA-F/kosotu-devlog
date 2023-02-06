46b25ff64e9c5dd5f0121aec51bcedbaa51b06a2
RYOTA-F <ry.fj0304@gmail.com>
2023-02-07 00:53:04 +0900

    :rotating_light: BlogBody

--- src/components/organisms/BlogBody/index.test.tsx
+++ src/components/organisms/BlogBody/index.test.tsx
@@ -6,18 +6,22 @@ import BlogContextProviderMock from '@/stores/__mocks__/blog'
 import { blogsMock } from '@/logic/usecase/microCMS/blog/__mocks__'
 
 describe('BlogBody', () => {
-  beforeEach(() => {
+  test('描画される', () => {
     render(
       <BlogContextProviderMock>
         <BlogBody />
       </BlogContextProviderMock>
     )
-  })
 
-  test('描画される', () => {
     const blogBodyElement = screen.getByLabelText(ARIA_LABEL)
 
     expect(blogBodyElement).toBeInTheDocument()
     expect(blogBodyElement.innerHTML).toEqual(blogsMock[0].body)
+  })
+
+  test('blog がない場合は表示されない', () => {
+    render(<BlogBody />)
+
+    expect(screen.queryByLabelText(ARIA_LABEL)).not.toBeInTheDocument()
   })
 })
