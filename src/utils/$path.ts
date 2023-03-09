cd342a4620cb6f6029c0205a6aa5cc980adbbaac
RYOTA-F <ry.fj0304@gmail.com>
2023-03-10 07:18:52 +0900

    :sparkles: Pathpida

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -1,74 +1,46 @@
 export const pagesPath = {
-  $404: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: '/404' as const,
-      hash: url?.hash,
-    }),
+  "$404": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash })
   },
-  blog: {
+  "blog": {
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({
-        pathname: '/blog/[id]' as const,
-        query: { id },
-        hash: url?.hash,
-      }),
-    }),
+      $url: (url?: { hash?: string }) => ({ pathname: '/blog/[id]' as const, query: { id }, hash: url?.hash })
+    })
   },
-  category: {
+  "category": {
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({
-        pathname: '/category/[id]' as const,
-        query: { id },
-        hash: url?.hash,
-      }),
-    }),
+      $url: (url?: { hash?: string }) => ({ pathname: '/category/[id]' as const, query: { id }, hash: url?.hash }),
+      _pageId: (pageId: string | number) => ({
+        $url: (url?: { hash?: string }) => ({ pathname: '/category/[id]/[pageId]' as const, query: { id, pageId }, hash: url?.hash })
+      })
+    })
   },
-  disclaimer: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: '/disclaimer' as const,
-      hash: url?.hash,
-    }),
+  "disclaimer": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/disclaimer' as const, hash: url?.hash })
   },
-  page: {
+  "page": {
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({
-        pathname: '/page/[id]' as const,
-        query: { id },
-        hash: url?.hash,
-      }),
-    }),
+      $url: (url?: { hash?: string }) => ({ pathname: '/page/[id]' as const, query: { id }, hash: url?.hash })
+    })
   },
-  privacy_policy: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: '/privacy-policy' as const,
-      hash: url?.hash,
-    }),
+  "privacy_policy": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/privacy-policy' as const, hash: url?.hash })
   },
-  profile: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: '/profile' as const,
-      hash: url?.hash,
-    }),
+  "profile": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/profile' as const, hash: url?.hash })
   },
-  sitemap: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: '/sitemap' as const,
-      hash: url?.hash,
-    }),
+  "sitemap": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/sitemap' as const, hash: url?.hash })
   },
-  tag: {
+  "tag": {
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({
-        pathname: '/tag/[id]' as const,
-        query: { id },
-        hash: url?.hash,
-      }),
-    }),
+      $url: (url?: { hash?: string }) => ({ pathname: '/tag/[id]' as const, query: { id }, hash: url?.hash }),
+      _pageId: (pageId: string | number) => ({
+        $url: (url?: { hash?: string }) => ({ pathname: '/tag/[id]/[pageId]' as const, query: { id, pageId }, hash: url?.hash })
+      })
+    })
   },
-  $url: (url?: { hash?: string }) => ({
-    pathname: '/' as const,
-    hash: url?.hash,
-  }),
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
 export type PagesPath = typeof pagesPath
