75d1e569b0e9541e23589e2f0b523566b8a582ff
RYOTA-F <ry.fj0304@gmail.com>
2023-03-23 06:45:10 +0900

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,22 +12,23 @@ import { googleTagManagerId } from '@/libs/gtag'
 import createEmotionServer from '@emotion/server/create-instance'
 import { cache } from '@emotion/css'
 
-export const renderStatic = (html: string) => {
-  if (html === undefined) {
-    throw new Error('did you forget to return html from renderToString?')
-  }
-  const { extractCritical } = createEmotionServer(cache)
-  const { ids, css } = extractCritical(html)
+// export const renderStatic = (html: string) => {
+//   if (html === undefined) {
+//     throw new Error('did you forget to return html from renderToString?')
+//   }
+//   const { extractCritical } = createEmotionServer(cache)
+//   const { ids, css } = extractCritical(html)
 
-  return { html, ids, css }
-}
+//   return { html, ids, css }
+// }
 
 export default class CustomDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
     const page = await ctx.renderPage()
-    const { css, ids } = renderStatic(page.html)
+    const { extractCritical } = createEmotionServer(cache)
+    const { ids, css } = extractCritical(page.html)
     const initialProps = await Document.getInitialProps(ctx)
 
     return {
