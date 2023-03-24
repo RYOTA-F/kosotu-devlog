f0b1e8a30ddd553491f3617f49e108ee81aa3cc2
RYOTA-F <ry.fj0304@gmail.com>
2023-03-25 04:54:59 +0900

    :recycle: Footer Tailwind

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,40 +3,30 @@ import Link from 'next/link'
 /* Const */
 import { SUB_MENU_LIST } from '@/const/index'
 import { FOOTER, ARIA_LABEL } from './const'
-/* Hooks */
-import useMediaQuery from '@/hooks/useMediaQuery'
-/* Styles */
-import {
-  FooterWrapper,
-  MenuList,
-  MenuItem,
-  MenuLabel,
-  CopyContainer,
-  CopyMark,
-  CopyText,
-} from './index.styles'
 
-const Footer: FC = () => {
-  const { isSP } = useMediaQuery()
-
-  return (
-    <FooterWrapper aria-label={ARIA_LABEL.FOOTER}>
-      <MenuList>
-        {SUB_MENU_LIST.map((v) => (
-          <MenuItem key={v.URL}>
-            <Link href={v.URL} target="_blank">
-              <MenuLabel isSP={isSP}>{v.LABEL}</MenuLabel>
-            </Link>
-          </MenuItem>
-        ))}
-      </MenuList>
-      <CopyContainer>
-        <CopyMark>{FOOTER.COPY_MARK}</CopyMark>
-        <CopyText>{FOOTER.COPY_TEXT}</CopyText>
-      </CopyContainer>
-    </FooterWrapper>
-  )
-}
+const Footer: FC = () => (
+  <footer
+    className="bg-gray-footer py-4 text-white"
+    aria-label={ARIA_LABEL.FOOTER}
+  >
+    <ul className="flex justify-center">
+      {SUB_MENU_LIST.map((v) => (
+        <li
+          key={v.URL}
+          className="border-r-[1px] px-3 first-of-type:border-l-[1px]"
+        >
+          <Link href={v.URL} target="_blank">
+            <span className="text-[14px]">{v.LABEL}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <div className="mt-4 flex items-center justify-center">
+      <span className="mr-1 text-base">{FOOTER.COPY_MARK}</span>
+      <span className="text-[12px]">{FOOTER.COPY_TEXT}</span>
+    </div>
+  </footer>
+)
 
 export default Footer
 export * from './const'
