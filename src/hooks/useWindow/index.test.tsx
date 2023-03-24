759ab670663e76d4ea870637be1b7047da6191c4
RYOTA-F <ry.fj0304@gmail.com>
2023-03-25 05:50:39 +0900

    :sparkles: useWindow

---
+++ src/hooks/useWindow/index.test.tsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+// import { renderHook, act } from '@testing-library/react'
+import '@testing-library/react'
+
+// import useWindow from './'
+
+describe('useWindow', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('windowWidth', () => {
+    test.todo('値が正しい')
+  })
+
+  describe('windowHeight', () => {
+    test.todo('値が正しい')
+  })
+
+  describe('isPC', () => {
+    test.todo('値が正しい')
+  })
+
+  describe('isTB', () => {
+    test.todo('値が正しい')
+  })
+
+  describe('isSP', () => {
+    test.todo('値が正しい')
+  })
+})
