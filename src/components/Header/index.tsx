b6b3da17985018498b606888708607955739fc2b
RYOTA-F <ry.fj0304@gmail.com>
2023-03-25 04:36:32 +0900

    :recycle: Header Tailwind

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,48 +2,43 @@ import { FC } from 'react'
 import Link from 'next/link'
 /* Components */
 import AccordionMenu from '@/components/AccordionMenu'
-// import { TwitterSvg } from '@/components/Elements/Svg'
+import { TwitterSvg } from '@/components/Elements/Svg'
 // import HamburgerMenu from '@/components/HamburgerMenu'
 // import Sidenav from '@/components/Sidenav'
 /* Const */
-import {
-  SITE,
-  PAGE,
-  // TWITTER
-} from '@/const/index'
-import {
-  //  HEADER,
-  ARIA_LABEL,
-} from './const'
+import { SITE, PAGE, TWITTER } from '@/const/index'
+import { HEADER, ARIA_LABEL } from './const'
 /* Hooks */
-import useMediaQuery from '@/hooks/useMediaQuery'
+// import useMediaQuery from '@/hooks/useMediaQuery'
 import useCommonData from '@/hooks/useCommonData'
 
 const Header: FC = () => {
   const { globalMenu } = useCommonData()
-  const {
-    isPC,
-    // isTB
-  } = useMediaQuery()
+  // const {
+  //   isPC,
+  //   isTB
+  // } = useMediaQuery()
 
   return (
     <>
-      {/* {isPC && (
-        <HeaderBar>
-          <CatchPrase>{HEADER.CATCH_PHRASE}</CatchPrase>
-          <IconList>
-            <Icon>
-              <Link href={TWITTER.URL} target="_blank">
-                <TwitterSvg
-                  height={HEADER.ICON.HEIGHT}
-                  width={HEADER.ICON.WIDTH}
-                  color={HEADER.ICON.COLOR}
-                />
-              </Link>
-            </Icon>
-          </IconList>
-        </HeaderBar>
-      )} */}
+      {/* {isPC && ( */}
+      <div className="flex justify-between h-[22px] px-[10%] bg-blue-main">
+        <span className="py-1 text-white text-[12px]">
+          {HEADER.CATCH_PHRASE}
+        </span>
+        <ul className="flex">
+          <li className="flex justify-center items-center py-1 h-[22px] w-[22px]">
+            <Link href={TWITTER.URL} target="_blank">
+              <TwitterSvg
+                height={HEADER.ICON.HEIGHT}
+                width={HEADER.ICON.WIDTH}
+                color={HEADER.ICON.COLOR}
+              />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/* )} */}
 
       <header
         className="sticky top-0 z-50 flex h-[72px] justify-between bg-blue-main px-[10%]"
@@ -55,22 +50,22 @@ const Header: FC = () => {
           </h1>
         </Link>
 
-        {isPC && (
-          <ul className="flex h-[100%] leading-[72px] text-white">
-            {globalMenu.map((v) => (
-              <li key={v.label} className="mx-3">
-                <AccordionMenu
-                  label={v.label}
-                  path={v.url}
-                  menuList={v.children.map((children) => ({
-                    label: children.label,
-                    path: children.url,
-                  }))}
-                />
-              </li>
-            ))}
-          </ul>
-        )}
+        {/* {isPC && ( */}
+        <ul className="flex h-[100%] leading-[72px] text-white">
+          {globalMenu.map((v) => (
+            <li key={v.label} className="mx-3">
+              <AccordionMenu
+                label={v.label}
+                path={v.url}
+                menuList={v.children.map((children) => ({
+                  label: children.label,
+                  path: children.url,
+                }))}
+              />
+            </li>
+          ))}
+        </ul>
+        {/*  )} */}
 
         {/* {!isPC && (
           <>
