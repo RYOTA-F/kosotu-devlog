046cc0d3b4ed13d48c41dfbb51f634b30e557d52
RYOTA-F <ry.fj0304@gmail.com>
2023-03-25 09:28:25 +0900

    :recycle: BlogCardList Tailwind

--- src/features/blogs/BlogCardList/index.tsx
+++ src/features/blogs/BlogCardList/index.tsx
@@ -5,17 +5,18 @@ import BlogCard from '@/features/blogs/BlogCard'
 import { ARIA_LABEL } from './const'
 /* Hooks */
 import useBlogData from '@/hooks/useBlogData'
-// import useMediaQuery from '@/hooks/useMediaQuery'
 
 const BlogCardList: FC = () => {
   const { blogs } = useBlogData()
-  // const { isSP } = useMediaQuery()
 
   return (
     <article aria-label={ARIA_LABEL.BLOG_CARD_LIST}>
-      <ul className="mt-[-8px] flex flex-wrap">
+      <ul className="mt-[-8px] flex flex-wrap sp:block">
         {blogs.map((v) => (
-          <li key={v.title} className="w-[48%] p-[1%]">
+          <li
+            key={v.title}
+            className="w-[48%] p-[2%] tb:p-[1%] sp:w-[95%] sp:p-0 sp:mx-auto sp:[&:not(:first-of-type)]:mt-4"
+          >
             <BlogCard title={v.title} id={v.id} image={v.image} />
           </li>
         ))}
