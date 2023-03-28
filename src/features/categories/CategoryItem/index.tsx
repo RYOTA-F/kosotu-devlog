ae9daf2e682c7b41676c4b2519296a5272e192ad
RYOTA-F <ry.fj0304@gmail.com>
2023-03-28 17:42:52 +0900

    :recycle: CategoryItem Tailwind

--- src/features/categories/CategoryItem/index.tsx
+++ src/features/categories/CategoryItem/index.tsx
@@ -2,14 +2,9 @@ import { FC } from 'react'
 import Link from 'next/link'
 /* Const */
 import { PAGE } from '@/const/index'
-/* Hooks */
-import useMediaQuery from '@/hooks/useMediaQuery'
-/* Styles */
-import { CategoryItemWrapper } from './index.styles'
+import { ARIA_LABEL } from './const'
 /* Types */
 import { ICategory } from '@/types/index'
-
-export const ARIA_LABEL = 'categoryItem' as const
 
 interface ICategoryItemParent {
   id: string
@@ -20,14 +15,19 @@ export interface ICategoryItem {
   category: ICategory | ICategoryItemParent
 }
 
-const CategoryItem: FC<ICategoryItem> = ({ category }) => {
-  const { isSP } = useMediaQuery()
-
-  return (
-    <CategoryItemWrapper isSP={isSP} aria-label={ARIA_LABEL}>
-      <Link href={`${PAGE.CATEGORY}${category.id}`}>{category.name}</Link>
-    </CategoryItemWrapper>
-  )
-}
+const CategoryItem: FC<ICategoryItem> = ({ category }) => (
+  <div
+    className="inline-block cursor-pointer overflow-hidden bg-blue-main border-transparent rounded-md border-[1px] box-border transition duration-400 text-[14px] hover:bg-white hover:border-blue-main"
+    aria-label={ARIA_LABEL}
+  >
+    <Link
+      href={`${PAGE.CATEGORY}${category.id}`}
+      className="block py-1 px-2 text-white hover:text-blue-main"
+    >
+      {category.name}
+    </Link>
+  </div>
+)
 
 export default CategoryItem
+export * from './const'
