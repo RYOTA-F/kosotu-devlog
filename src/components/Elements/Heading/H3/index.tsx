944e3c058ccc785de77285dbb6ceaa75f59e3c21
RYOTA-F <ry.fj0304@gmail.com>
2023-03-29 22:44:59 +0900

    :recycle: H3 Tailwind

--- src/components/Elements/Heading/H3/index.tsx
+++ src/components/Elements/Heading/H3/index.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react'
 /* Const */
 import { ARIA_LABEL } from '../const'
-/* Styles */
-import { Wrapper } from './index.styles'
 /* Types */
 import { IHeadingDefault } from '../types'
 
-const H4: FC<IHeadingDefault> = ({ children }) => {
-  return <Wrapper aria-label={ARIA_LABEL.H4}>{children}</Wrapper>
+const H3: FC<IHeadingDefault> = ({ children }) => {
+  return (
+    <h3
+      className="mt-10 mb-7 py-1 px-4 text-gray-text-t4 bg-gray-list font-bold border-l-4 border-blue-main"
+      aria-label={ARIA_LABEL.H3}
+    >
+      {children}
+    </h3>
+  )
 }
 
-export default H4
+export default H3
