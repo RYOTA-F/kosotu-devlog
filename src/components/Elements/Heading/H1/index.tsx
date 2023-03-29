c11a3f62de2b67613a2647b88b25e56185310410
RYOTA-F <ry.fj0304@gmail.com>
2023-03-29 12:23:51 +0900

    :recycle: H1 Tailwind

--- src/components/Elements/Heading/H1/index.tsx
+++ src/components/Elements/Heading/H1/index.tsx
@@ -1,13 +1,19 @@
 import { FC } from 'react'
 /* Const */
 import { ARIA_LABEL } from '../const'
-/* Styles */
-import { Wrapper } from './index.styles'
 /* Types */
 import { IHeadingDefault } from '../types'
 
 const H1: FC<IHeadingDefault> = ({ children }) => {
-  return <Wrapper aria-label={ARIA_LABEL.H1}>{children}</Wrapper>
+  return (
+    <h1
+      className="mb-[60px] pl-4 text-gray-text-t3 text-[24px] font-bold relative"
+      aria-label={ARIA_LABEL.H1}
+    >
+      {children}
+      <div className="absolute content-[''] w-full -bottom-[10px] left-0 h-[2px] bg-gradient-to-r from-blue-main" />
+    </h1>
+  )
 }
 
 export default H1
