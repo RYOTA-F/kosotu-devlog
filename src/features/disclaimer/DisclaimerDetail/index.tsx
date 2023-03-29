160bf9ba079e44cf84e59438b0e7e33665e28e51
RYOTA-F <ry.fj0304@gmail.com>
2023-03-29 11:40:10 +0900

    :recycle: DisclaimerDetail Tailwind

--- src/features/disclaimer/DisclaimerDetail/index.tsx
+++ src/features/disclaimer/DisclaimerDetail/index.tsx
@@ -3,37 +3,43 @@ import { FC } from 'react'
 import { H1, H3 } from '@/components/Elements/Heading'
 /* Const */
 import { DISCLAIMER, ARIA_LABEL } from './const'
-/* Styles */
-import { Wrapper, Description, Content, List, ListItem } from './index.styles'
 
 const DisclaimerDetail: FC = () => {
   return (
-    <Wrapper aria-label={ARIA_LABEL.DISCLAIMER_DETAIL}>
+    <div
+      className="w-full text-gray-text-t3"
+      aria-label={ARIA_LABEL.DISCLAIMER_DETAIL}
+    >
       <H1>{DISCLAIMER.TITEL}</H1>
-      <Description>{DISCLAIMER.DESCRIPTION}</Description>
+      <p className="my-[60px] leading-[1.8]">{DISCLAIMER.DESCRIPTION}</p>
       {/* コメント */}
       <H3>{DISCLAIMER.COMMENT.TITLE}</H3>
-      <Content>
+      <div className="leading-[1.8]">
         {DISCLAIMER.COMMENT.CONTENT}
-        <List>
+        <ul className="my-8 pl-6">
           {DISCLAIMER.COMMENT.LIST.map((v) => (
-            <ListItem key={v}>{v}</ListItem>
+            <li key={v} className="relative">
+              <span className="absolute -left-5 font-bold">
+                {DISCLAIMER.LIST.POINTER}
+              </span>
+              {v}
+            </li>
           ))}
-        </List>
-      </Content>
+        </ul>
+      </div>
       {/* 情報 */}
       <H3>{DISCLAIMER.INFORMATION.TITLE}</H3>
-      <Content>{DISCLAIMER.INFORMATION.CONTENT}</Content>
+      <div className="leading-[1.8]">{DISCLAIMER.INFORMATION.CONTENT}</div>
       {/* 損害等の責任 */}
       <H3>{DISCLAIMER.RESPONSIBILITY.TITLE}</H3>
-      <Content>{DISCLAIMER.RESPONSIBILITY.CONTENT}</Content>
+      <div className="leading-[1.8]">{DISCLAIMER.RESPONSIBILITY.CONTENT}</div>
       {/* 著作権・肖像権 */}
       <H3>{DISCLAIMER.COPYRIGHT.TITLE}</H3>
-      <Content>{DISCLAIMER.COPYRIGHT.CONTENT}</Content>
+      <div className="leading-[1.8]">{DISCLAIMER.COPYRIGHT.CONTENT}</div>
       {/* 無断転載 */}
       <H3>{DISCLAIMER.REPRINT.TITLE}</H3>
-      <Content>{DISCLAIMER.REPRINT.CONTENT}</Content>
-    </Wrapper>
+      <div className="leading-[1.8]">{DISCLAIMER.REPRINT.CONTENT}</div>
+    </div>
   )
 }
 
