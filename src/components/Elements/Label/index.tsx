9df774ad00925c26ad4c26c4e32d5fcd231ad175
RYOTA-F <ry.fj0304@gmail.com>
2023-03-29 22:49:49 +0900

    :recycle: Label Tailwind

--- src/components/Elements/Label/index.tsx
+++ src/components/Elements/Label/index.tsx
@@ -1,15 +1,20 @@
 import { FC, ReactNode } from 'react'
 /* Const */
 import { ARIA_LABEL } from './const'
-/* Styles */
-import { Wrapper } from './index.styles'
 
 export interface ILabel {
   children: ReactNode
 }
 
 const Label: FC<ILabel> = ({ children }) => {
-  return <Wrapper aria-label={ARIA_LABEL}>{children}</Wrapper>
+  return (
+    <p
+      className="inline-block py-3 px-4 text-gray-text-t4 bg-white border-l-8 border-blue-main font-bold shadow-md"
+      aria-label={ARIA_LABEL}
+    >
+      {children}
+    </p>
+  )
 }
 
 export default Label
